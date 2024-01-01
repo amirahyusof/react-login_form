db69f5c889df66b72ff237e162b1c980570240cb
amirah <mirez.data@gmail.com>
2024-01-01 23:13:54 +0800

    commit

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -5,64 +5,82 @@ function Page() {
   const [formData, setFormData] = React.useState({
     email: "",
     password: "",
-    confirmPassword: ""
+    passwordConfirm: ""
   })
 
   function handleChange(event){
     setFormData( prevFormData => {
       return {
         ...prevFormData, 
-       [event.target.name] : [event.target.value]
+       [event.target.name] : event.target.value
       }
     })
   }
 
-  console.log(formData)
 
   function handleSubmit(event){
     event.preventDefault()
+
+    if(formData.password === formData.passwordConfirm){
+      console.log("Password match");
+
+    } else {
+      console.log("Passwords do not match")
+
+    }
+
+    if(formData.email === ""){
+      console.log("Please write your email")
+    
+    } else {
+      console.log("Good job")
+      return
+    }
+    
   }
+
+  console.log(formData)
 
   return (
     <div className='box--container'>
-       <div className="login--container" onSubmit={handleSubmit}>
-      <h1 className='page--header'>Login</h1>
+       <form className="login--container" onSubmit={handleSubmit}>
+        <h1 className='page--header'>Login</h1>
   
-      <label>Email</label>
-      <input 
-      type='email'
-      placeholder='Enter your email'
-      className='page--input'
-      name='email'
-      value={formData.email}
-      onChange ={handleChange}
-      />
-      <br></br>
+        <label>Email</label>
+        <input 
+        type='email'
+        placeholder='Enter your email'
+        className='page--input'
+        name='email'
+        value={formData.email}
+        onChange ={handleChange}
+        />
+        <br></br>
 
-      <label>Password</label>
-      <input
-      type='password'
-      placeholder='Password'
-      className='page--input'
-      name='password'
-      value={formData.password}
-      onChange={handleChange} 
-      />
-      <br></br>
+        <label>Password</label>
+        <input
+        type='password'
+        placeholder='Password'
+        className='page--input'
+        name='password'
+        value={formData.password}
+        onChange={handleChange} 
+        />
+        <br></br>
 
-      <label>Confirm Password</label>
-      <input 
-      type='password'
-      placeholder='Confirm Password' 
-      className='page--input'
-      name='confirmPassword'
-      value={formData.confirmPassword}
-      onChange={handleChange}
-      />
-      <br></br>
+        <label>Confirm Password</label>
+        <input 
+        type='password'
+        placeholder='Confirm Password' 
+        className='page--input'
+        name='passwordConfirm'
+        value={formData.passwordConfirm}
+        onChange={handleChange}
+        />
+        <br></br>
 
-      <button type='submit' className='button--signIn'>Sign in</button>
-    </div>
+        <button type='submit' className='button--signIn'>Sign in</button>
+      </form>
     </div>
    
   );
