8f554f82d896e85fdad3dbb9210cc99c4d2956a9
amirah <mirez.data@gmail.com>
2024-01-03 22:42:58 +0800

    commit

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './LoginPage.css';
+import { useForm } from 'react-hook-form'
 
 function Page() {
   const [formData, setFormData] = React.useState({
@@ -8,34 +9,39 @@ function Page() {
     passwordConfirm: ""
   })
 
+  const {register, handleSubmit, formState:{ errors }} = useForm()
+
   function handleChange(event){
+    const {name, value} = event.target
     setFormData( prevFormData => {
       return {
         ...prevFormData, 
-       [event.target.name] : event.target.value
+       [name] : value
       }
     })
   }
 
 
-  function handleSubmit(event){
+  function onSubmit(event) {
     event.preventDefault()
 
     if(formData.password === formData.passwordConfirm){
-      console.log("Password match");
+      alert("Password match");
 
     } else {
-      console.log("Passwords do not match")
+      alert("Passwords do not match")
 
     }
 
     if(formData.email === ""){
-      console.log("Please write your email")
+      alert("Please write your email")
     
     } else {
-      console.log("Good job")
+      alert("Thank you")
       return
     }
+
+    alert("Thank you for login")
     
   }
 
@@ -43,41 +49,51 @@ function Page() {
 
   return (
     <div className='box--container'>
-       <form className="login--container" onSubmit={handleSubmit}>
+       <form className="login--container" onSubmit={handleSubmit(onSubmit)}>
         <h1 className='page--header'>Login</h1>
   
+        <div>
         <label>Email</label>
         <input 
         type='email'
         placeholder='Enter your email'
         className='page--input'
+        {...register("email", {required: true, pattern:/^\S+@\S+$/i} )}
         name='email'
         value={formData.email}
         onChange ={handleChange}
         />
-        <br></br>
+        {errors.email && <p>Email is required and must be valid</p>}
+        </div>
 
+        <div>
         <label>Password</label>
         <input
         type='password'
         placeholder='Password'
         className='page--input'
         name='password'
+        {...register("password", {require:true} )}
         value={formData.password}
         onChange={handleChange} 
         />
-        <br></br>
+        {errors.password && <p>Password is require</p>}
+        </div>
 
+        <div>
         <label>Confirm Password</label>
         <input 
         type='password'
         placeholder='Confirm Password' 
         className='page--input'
+        {...register("passwordConfirm", {require:true})}
         name='passwordConfirm'
         value={formData.passwordConfirm}
         onChange={handleChange}
         />
-        <br></br>
+        {errors.passwordConfirm && <p>Password does not match</p>}
+        </div>
+    
 
         <button type='submit' className='button--signIn'>Sign in</button>
       </form>
