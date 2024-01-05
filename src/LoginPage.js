743608aa57774d0cf099283a42b9008bce495071
amirah <mirez.data@gmail.com>
2024-01-05 23:01:44 +0800

    commit

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './LoginPage.css';
-import { useForm } from 'react-hook-form'
 
 function Page() {
   const [formData, setFormData] = React.useState({
@@ -9,78 +8,127 @@ function Page() {
     passwordConfirm: ""
   })
 
-  const { register, handleSubmit, formState: {errors} } = useForm()
+  const [inputError, setInputError] = React.useState({
+    email: "",
+    password: "",
+    passwordConfirm: ""
+  })
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 
   function handleChange(event){
     const {name, value} = event.target
+
     setFormData( prevFormData => {
       return {
         ...prevFormData, 
        [name] : value
       }
     })
+
+    if(name === 'email'){
+      if(value.match(validRegex)){
+        setInputError(prevInputError => ({
+          ...prevInputError, 
+          [name]: null
+        }))
+      } else {
+        setInputError(prevInputError => ({
+          ...prevInputError, 
+          [name]: "Please enter valid email"
+        }))
+    }
+  } 
+  
+  else if(name === 'password'){
+    if(value.length >= 6){
+      setInputError(prevInputError =>({
+        ...prevInputError, 
+        [name]:null
+      }))
+    }
+   else {
+    setInputError(prevInputError => ({
+      ...prevInputError, 
+      [name]: "Password must be at least 6 characters"
+    }))
+    } 
   }
 
+  else if (name === 'passwordConfirm'){
+    if(value === formData.password){
+      setInputError(prevInputError => ({
+        ...prevInputError, 
+        [name]: null
+      }))
+    } else {
+    setInputError(prevInputError => ({
+      ...prevInputError, 
+      [name]: "Password do not match"
+      }))
+    }
+  }
+}
 
-  const onSubmit = (data) => {
-    console.log(data)
-    alert("Thank you for login")
-    
+
+  function handleSubmit(event){
+    event.preventDefault()
+
+    if(formData.password !== formData.passwordConfirm){
+      setInputError(prevInputError => ({
+        ...prevInputError, 
+        passwordConfirm: 'Password do not match'
+      }))
+    } else {
+      //procedd with submission
+      alert("Thank you for logging in")
+    }
+
   }
 
   console.log(formData)
 
   return (
     <div className='box--container'>
-       <form className="login--container" onSubmit={handleSubmit(onSubmit)}>
+       <form className="login--container" onSubmit={handleSubmit}>
         <h1 className='page--header'>Login</h1>
-  
-        <div>
+
         <label>Email</label>
+        <div className='page--input'>
+        <br></br>
         <input 
         type='email'
         placeholder='Enter your email'
-        className='page--input'
-        {...register("email", {required: true, pattern:/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/} )}
         name='email'
         value={formData.email}
         onChange ={handleChange}
         />
-        {errors.email && errors.email.type === 'required' 
-        && (<p>Email is required</p>)}
-        {errors.email && errors.email.type === 'pattern' 
-        && (<p>Email is not valid</p>)}
+        {inputError.email && <div style={{color:'red'}}>{inputError.email}</div>}
         </div>
 
-        <div>
         <label>Password</label>
+        <div className='page--input'>
+        <br></br>
         <input
         type='password'
         placeholder='Password'
-        className='page--input'
         name='password'
-        {...register("password", {required:true, minLength: 6} )}
         value={formData.password}
         onChange={handleChange} 
         />
-        {errors.password &&  errors.password.type === 'required'
-        && (<p>Password is require</p>)}
-        {errors.password && errors.password.type === 'minLength'
-        && (<p>Password should be at least 6 characters</p>)}
+        {inputError.password && <div style={{color: 'red'}}>{inputError.password}</div>}
         </div>
 
-        <div>
         <label>Confirm Password</label>
+        <div className='page--input'>
         <input 
         type='password'
-        placeholder='Confirm Password' 
-        className='page--input'
-        {...register("passwordConfirm", {required:true})}
+        placeholder='Confirm Password'
         name='passwordConfirm'
         value={formData.passwordConfirm}
         onChange={handleChange}
         />
-        {errors.passwordConfirm && <p>Password does not match</p>}
+        {inputError.passwordConfirm && <div style={{color: 'red'}}>{inputError.passwordConfirm}</div>}
         </div>
     
 
